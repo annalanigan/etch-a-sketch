6b251bf9945e2c9f1df30fdcc06e068aa62dcae6
annalanigan <annalanigan@outlook.com>
2018-02-01 12:20:02 +0000

    git commit

---
+++ public/index.js
@@ -0,0 +1,63 @@
+window.addEventListener('load', function(){
+
+var canvas = document.querySelector('#main-canvas');
+var context = canvas.getContext('2d');
+var oldX = 100;
+var oldY = 100;
+
+  var setX= function(x){
+  if (x < 590 && x > 10)
+    oldX = x;
+  }
+
+  var setY = function(y){
+    if (y < 490 && y > 10)
+      oldY = y;
+  }
+
+  var setNewCoods = function(x, y){
+    setX(x);
+    setY(y);
+  }
+
+context.beginPath();
+context.moveTo(oldX, oldY);
+
+
+var newCoordinates = function(event, x, y){
+  // console.log("whats the key", event.key);
+  // console.log('what is y initially', y);
+  switch (event.key) {
+    case 'ArrowDown':
+      // console.log('what was y', y);
+      y+= 5;
+      // console.log('whats y now', y);
+      draw(x, y);
+      break;
+    case 'ArrowUp':
+      y-= 5;
+      draw(x, y);
+      break;
+    case 'ArrowLeft':
+      x-= 5;
+      draw(x, y);
+      break;
+    case 'ArrowRight':
+      x+= 5;
+      draw(x, y);
+      break;
+  }
+}
+
+var draw = function(x, y) {
+  context.lineTo(x, y);
+  context.stroke();
+  setNewCoods(x ,y);
+}
+
+document.addEventListener('keydown', function(event){
+  console.log('hello?', event);
+  newCoordinates(event, oldX, oldY);
+})
+
+})
